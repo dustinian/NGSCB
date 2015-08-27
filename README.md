b401f8f3435c06b8cab3b4071a2a345b2899bd2d
Dustinian <dustinian@gmail.com>
2015-08-27 16:50:50 -0400

    Updated README.md; added create_tables.sql.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# NGSCB
-Narrative Game System Character Builder
+# Narrative Game System Character Builder (NGSCB)
+
+##Purpose
+
+This is a character builder for the "Narrative Game System" by [Venture Land Games](http://www.venturelandgames.com).
+
+##Guiding Principle
+
+>"9. Smart data structures and dumb code works a lot better than the other way around." - Eric S. Raymond, [The Cathedral and the Bazaar](http://www.catb.org)
+
+I emphasized the tables and queries in NGCSB much more than the procedures. NGSCB is a database, first and foremost.
+
+##Tables
+
---
+++ create_tables.sql
@@ -0,0 +1,34 @@
+﻿Create Table tblAccess
+	intAccessID as Integer,
+	txtDescription as Char(10),
+	bolInsert as Boolean,
+	bolUpdate as Boolean,
+	bolDelete as Boolean,
+	bolSelect as Boolean;
+
+Create Table tblUser
+	intAccessID as Integer,			--Inherit: Parent
+	intUserID as Integer,			--Automat: ID
+	txtUsername as Varchar(20),		--User: 
+	txtName as VarChar(75),
+	txtPassword as Varchar(100),
+	txtEmail as Varchar(150),
+	datJoined as Date,
+	timJoined as Time,
+	bolActive as Boolean,
+	bolVerified as Boolean;
+
+Create Table tblSetting
+	intUserID as Integer,
+	intShareID as Integer,
+	
+
+Create Table tblCampaign
+	intUserID as Integer,
+	intCampaignStatus as Integer,
+	intCampaignID as Integer,
+	txtName as Varchar,
+	txtDescription as Varchar,
+	datCreated as Date,
+	timCreated as Time,
+	bolPublish as Boolean;
